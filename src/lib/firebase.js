1ee55d190a6dde649733ab65cbffd28f78f6e679
aditya00604 <4si21cs006@sit.ac.in>
2024-08-10 14:18:31 +0530

    commmit rebase deleted commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,15 +3,16 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDCCDH3ZOPkPnPiO84hNWhZb8VPFSaPCYQ",
-    authDomain: "radiant-raceway-428719-v0.firebaseapp.com",
-    databaseURL: "https://radiant-raceway-428719-v0-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "radiant-raceway-428719-v0",
-    storageBucket: "radiant-raceway-428719-v0.appspot.com",
-    messagingSenderId: "935368220795",
-    appId: "1:935368220795:web:9264bfbc9acc2ef09fc2de",
-    measurementId: "G-FGG9XPHZEJ"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
 
 const app = initializeApp(firebaseConfig);
 
